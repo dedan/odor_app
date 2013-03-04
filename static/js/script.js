01cfc201dbfaf84241fd7800fc09b693b5eb48ed
Stephan Gabler <stephan.gabler@gmail.com>
2013-03-04 15:01:51 +0100

    immediately display first receptor

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,12 @@
 
-
-$("select").change(function() {
+reload_data = function() {
     url = $("select.receptor").val() + '/' + $("select.method").val()
     console.log(url)
     $("select.receptor").val() + '/' + $("select.method").val()
     $.get(url, function(data) {
         $('div#container').html(data);
     });
-});
+}
+
+$("select").change(reload_data);
+$(reload_data)
