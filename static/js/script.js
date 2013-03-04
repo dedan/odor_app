a8826836d5100da65da1f162b899f75f000e8de1
Stephan Gabler <stephan.gabler@gmail.com>
2013-03-04 18:28:42 +0100

    add global data structure

--- static/js/script.js
+++ static/js/script.js
@@ -1,17 +1,20 @@
-var data = []
+var data_container = {predictions: [], count: 0}
 
 function reload_data() {
         url = $("select.receptor").val() + '/' + $("select.method").val()
         console.log(url)
         $("select.receptor").val() + '/' + $("select.method").val()
         $.getJSON(url, function(response) {
-            data = response;
+            data_container.predictions = response.predictions;
+            data_container.count = 10
+            $('#score').html(response.q2_score)
             console.log('data received');
-            render_table(data.predictions.slice(0, 50))
+            render_table()
         });
     }
 
-function render_table(tmp_data) {
+function render_table() {
+    tmp_data = data_container.predictions.slice(0, data_container.count)
     var rows = d3.select("tbody").selectAll("tr").data(tmp_data)
     rows.enter().append("tr")
     rows.exit().remove()
